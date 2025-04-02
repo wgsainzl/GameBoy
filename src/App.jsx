0edb62f61b395e3b6f4502417fc70b7a6a4b93c9
mauriciovillalobos05 <maugva05@hotmail.com>
2025-04-01 20:52:43 -0600

    FirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,121 @@
+import { createElement } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {/* container game */}
+        <div
+          style={{
+            width: "350px",
+            height: "500px",
+            border: "2px black solid",
+            borderRadius: "5px 5px 35px 5px",
+          }}
+        >
+          {/* container screen */}
+          <div
+            style={{
+              paddingTop: "5%",
+              paddingBottom: "25%",
+              justifyContent: "center",
+              display: "flex",
+            }}
+          >
+            <div
+              style={{
+                width: "90%",
+                height: "200px",
+                backgroundColor: "olive",
+              }}
+            ></div>
+          </div>
+          {/* container buttons */}
+          <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <div
+              style={{
+                width: "60px",
+                height: "60px",
+                backgroundColor: "#C8C5CA",
+                borderRadius: "20px 20px 20px 20px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <div>
+                <button
+                  style={{
+                    backgroundColor: "black",
+                    width: "45px",
+                    height: "45px",
+                  }}
+                ></button>
+              </div>
+            </div>
+            <div style={{ paddingTop: "30%" }}>
+              <div
+                style={{
+                  width: "60px",
+                  height: "60px",
+                  backgroundColor: "gray",
+                  display:"flex",
+                  alignItems:"center" 
+                }}
+              >
+                <button
+                  style={{
+                    height: "1px",
+                    width: "30px",
+                    transform: "rotate(150deg",
+                  }}
+                ></button>
+                <button
+                  style={{
+                    height: "1px",
+                    width: "30px",
+                    transform: "rotate(150deg",
+                  }}
+                ></button>
+              </div>
+            </div>
+            {/* botones A y B */}
+            <div
+              style={{
+                width: "60px",
+                height: "60px",
+                display: "flex",
+                backgroundColor: "black",
+                borderRadius: "10px 10px 10px 10px",
+              }}
+            >
+              <div>
+                <button
+                  style={{
+                    backgroundColor: "#821660",
+                    borderRadius: "50%",
+                    height: "40px",
+                    width: "40px",
+                  }}
+                ></button>
+              </div>
+              <div>
+                <button
+                  style={{
+                    backgroundColor: "#821660",
+                    borderRadius: "50%",
+                    height: "40px",
+                    width: "40px",
+                  }}
+                ></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
